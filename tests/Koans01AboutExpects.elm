b7d5169b4827e9bdbb50f4ffa8eda2a0d478a154
Derek Shoemaker <shoemakerdr@gmail.com>
2018-02-22 13:45:54 -0600

    Finished koans 01

--- tests/Koans01AboutExpects.elm
+++ tests/Koans01AboutExpects.elm
@@ -13,29 +13,30 @@ import Expect
 
 -- members from other modules can be individually exposed within this module
 
-import Utils.Test exposing (describe, test)
+import Utils.Test as T exposing (describe, test)
 
 
 -- or you may expose all the members of another module
 
-import Utils.Blank exposing (..)
 
-
+testSuite : T.Test
 testSuite =
     -- `describe` takes the suite description and a list of `Test`s
     describe "About Expects"
         [ -- `test` takes the test description and a function that returns an
           -- `Expectation` when evaluated with the unit tuple `()`
           test "Expect.true tests for a true value"
-            (\() -> Expect.true "Should be True" (x____replace me____x))
-          -- `<|` calls the function on the left with the argument on the right
-          --  and can be used to avoid some parenthesis
+            (\() -> Expect.true "Should be True" (True))
+
+        -- `<|` calls the function on the left with the argument on the right
+        --  and can be used to avoid some parenthesis
         , test "Expect.equal tests for equality" <|
-            \() -> Expect.equal True (x____replace me____x)
-          -- `|>` calls the function on the right with the argument on the left
-          -- and can be used to "pipeline" values through a series of functions
+            \() -> Expect.equal True (True)
+
+        -- `|>` calls the function on the right with the argument on the left
+        -- and can be used to "pipeline" values through a series of functions
         , test "Expect.notEqual tests for inequality" <|
             \() ->
-                x____replace me____x
+                True
                     |> Expect.notEqual False
         ]
